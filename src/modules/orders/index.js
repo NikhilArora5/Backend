843f85afbe1082fc574a91642bc28c30edcb309a
Nikhil <aroranikhil8184@gmail.com>
2023-09-23 10:15:40 +0530

    Payment verification & saving in db

--- src/modules/orders/index.js
+++ src/modules/orders/index.js
@@ -1,50 +1,8 @@
-const express=require("express")
-const router=express.Router()
-const {razorPay_instance}=require("../../utils/payment")
+const express = require("express");
+const router = express.Router();
+const {createOrder,transactionCheck}=require("./controllers/createOrder")
 
+router.post("/order", createOrder);
+router.post("/transactionCheck", transactionCheck);
 
-
-
-
-const createOrder=async(req,res)=>{
-  try {
-    let amount=req.body.amount
-    console.log("---body-----------",req.body)
-
-
-
-const options={
-    amount: amount*100,
-    currency: "INR",
-    receipt: "receipt#1",
-    
-  }
-
-
-const myOrder= await razorPay_instance.orders.create(options)
-
-if(myOrder){
-
-   return res.status(200).json({
-        data:myOrder,
-        status:200,
-        message:"order success"
-    })
-}
-else{
-    return res.status(400).json({
-        data:{},
-        status:400,
-        message:"order failed"
-    })
-}
-    
-  } catch (error) {
-    console.log("----ERORRR",error.message)
-  }
-
-}
-
-router.post("/order",createOrder)
-
-module.exports=router
+module.exports = router;
